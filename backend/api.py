864c016ea2e0ae9bcf6f6fe3e583f35335ba83a9
justv <justv@vt.edu>
2021-02-27 18:27:34 -0500

    added more methods

--- backend/api.py
+++ backend/api.py
@@ -3,6 +3,7 @@ import json
 from venmo_api import Client
 import cv2
 import pymongo
+import bson.json_util
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -100,17 +101,31 @@ def host_confirm_request():
 
 #app.run()
 
-def create_session_on_db(username, num_users):
+def create_connection(hostname, items, num_users, name):\
+    item_id = 0
+    uuid = create_session_on_db(hostname, num_users, name)
+    for (item in items):
+        #??? how to translate "items"
+        add_item_to_session(uuid, item[0], item[1], item_id)
+        id += 1
+    return cursor_to_json(sessions.find({"uuid": uuid}))
+
+def create_session_on_db(username, num_users, name):
     uuid = sessions.count() + 1
-    sessions.insert_one([{"uuid": uuid, "num_users":num_users, "num_items":0, "host": username, "items": [], "users": []}])
+    sessions.insert_one([{"uuid": uuid, "name:" name, "num_users":num_users, "host": username, "items": [], "users": []}])
     return uuid
 
-def add_item_to_session(session_id, name, price):
+def add_item_to_session(session_id, name, price, id):
     #??? IDS
-    sessions.update({"uuid": session_id}, {"$push": {"items": {"Name": name, "Price": price}}}) 
+    sessions.update({"uuid": session_id}, {"$push": {"items": {"id": id "Name": name, "Price": price}}}) 
 
 def add_user_to_session(session_id, name):
     sessions.update({"uuid": session_id}, {"$push": {"users": {"Name": name, "bought_items": []}}})
     sessions.update({"uuid": session_id}, { "$inc": {"num_users": 1}})
+
+def add_item_to_user(hostname)
+
+def cursor_to_json(cursor):
+    return dumps(list(cursor), indent = 2)
 
         
