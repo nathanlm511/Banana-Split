bd3958900d1433cd0ab776ffc2f5ac26fc1b0fa2
sschoedel <sschoedel@vt.edu>
2021-02-28 02:54:30 -0500

    parse

--- backend/api.py
+++ backend/api.py
@@ -6,7 +6,11 @@ import pymongo
 from flask_cors import CORS, cross_origin
 import numpy as np
 from bson.json_util import dumps
-from twilio.rest import Client
+from twilio.rest import Client # sms
+from find_corners import get_corner_points # receipts
+from parse_receipt import parse_receipt
+from preprocess import process_image_for_ocr
+from orient_receipt import orient_receipt
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -25,21 +29,46 @@ twilio_client = Client("AC588038219fa0772239a1af667dabd171", "771d5391ee31d506da
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Banana Split</h1><p>Divide receipt costs among friends with ease!</p>"
 
 @app.route('/test_image', methods=['POST'])
 def post_image():
-    """ post image and return the response """
+    """ post image and return the response """ 
     #filestr = request.files['file']
     #npimg = np.fromfile(filestr, np.uint8)
-    #img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    #cv2.imshow('image', img)
-    #cv2.waitKey(0)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    # if image has been preprocessed
+    return parse_receipt(img)
 
-    #decode_ung
+    # # else if img is unprocessed
+    # points = get_corner_points(img)
+
+    # orig_shape = (img.shape[0], img.shape[1])
+
+    # img_crop = cv2.resize(img, (600,840))
+    # # cv2.imshow('points found', img_crop)
+    # # cv2.waitKey(0)
+
+    # if len(points) == 4:
+    #     print("Orienting receipt")
+    #     # img must be same size as image used for get_corner_points
+    #     transformed_img, ratio = orient_receipt(points, img, orig_shape)
+        
+    #     transformed_img = cv2.resize(transformed_img, (int(840*ratio),840))
+    #     transformed_img = cv2.cvtColor(transformed_img, cv2.COLOR_BGR2GRAY)
+    #     edge_crop = 3
+    #     cropped_img = transformed_img[edge_crop:transformed_img.shape[0]-edge_crop, edge_crop:transformed_img.shape[1]-edge_crop]
+        
+    #     # processed_image = process_image_for_ocr(cropped_img)
+    #     # processed_image = cv2.resize(processed_image, (int(840*ratio),840))
+        
+    #     data = parse_receipt(cropped_img)
+    #     print(data)
+    # else:
+    #     print(f"Found {len(points)} points, need 4")
     
-    dummy_data = '{"balance due": "65.32", "all food": [{"name": "ACTIVIA MC BERRY 4PK M", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "B&J FOG BRWNIE ICM", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "B&) STRAW CHSCAKE ICM", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "SK WLD AK PINK SLMN Ax", "num items": 1, "item_cost": 4.49, "total cost": 4.49, "food group": "GROCERY"}, {"name": "FL 41-50 RAW SHRIMP M Ax", "num items": 3, "item_cost": 5.49, "total cost": 16.47, "food group": "MEAT"}, {"name": "FL ORIGINAL MEATBALL Ax", "num items": 1, "item_cost": 4.49, "total cost": 4.49, "food group": "MEAT"}, {"name": "BNLS NY STRIP 17S TH A x", "num items": 2, "item_cost": 7.29, "total cost": 15.280000000000001, "food group": "MEAT"}, {"name": "MSSLS GRLC BTTR SCE Ax", "num items": 1, "item_cost": 3.99, "total cost": 3.99, "food group": "MEAT"}, {"name": "GREEN ONIONS", "num items": 6, "item_cost": 0.79, "total cost": 4.74, "food group": "PRODUCE"}, {"name": "ORGANIC CELLO CARROT Ax", "num items": 1, "item_cost": 1.29, "total cost": 1.29, "food group": "PRODUCE"}, {"name": "WHOLE WHITE MUSHROOM A x", "num items": 1, "item_cost": 1.99, "total cost": 1.99, "food group": "PRODUCE"}, {"name": "MUSCADINE GRAPES ORT Ax", "num items": 1, "item_cost": 3.49, "total cost": 3.49, "food group": "PRODUCE"}]}'
-    return dummy_data
+    # dummy_data = '{"balance due": "65.32", "all food": [{"name": "ACTIVIA MC BERRY 4PK M", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "B&J FOG BRWNIE ICM", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "B&) STRAW CHSCAKE ICM", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "SK WLD AK PINK SLMN Ax", "num items": 1, "item_cost": 4.49, "total cost": 4.49, "food group": "GROCERY"}, {"name": "FL 41-50 RAW SHRIMP M Ax", "num items": 3, "item_cost": 5.49, "total cost": 16.47, "food group": "MEAT"}, {"name": "FL ORIGINAL MEATBALL Ax", "num items": 1, "item_cost": 4.49, "total cost": 4.49, "food group": "MEAT"}, {"name": "BNLS NY STRIP 17S TH A x", "num items": 2, "item_cost": 7.29, "total cost": 15.280000000000001, "food group": "MEAT"}, {"name": "MSSLS GRLC BTTR SCE Ax", "num items": 1, "item_cost": 3.99, "total cost": 3.99, "food group": "MEAT"}, {"name": "GREEN ONIONS", "num items": 6, "item_cost": 0.79, "total cost": 4.74, "food group": "PRODUCE"}, {"name": "ORGANIC CELLO CARROT Ax", "num items": 1, "item_cost": 1.29, "total cost": 1.29, "food group": "PRODUCE"}, {"name": "WHOLE WHITE MUSHROOM A x", "num items": 1, "item_cost": 1.99, "total cost": 1.99, "food group": "PRODUCE"}, {"name": "MUSCADINE GRAPES ORT Ax", "num items": 1, "item_cost": 3.49, "total cost": 3.49, "food group": "PRODUCE"}]}'
+    # return dummy_data
 
 # When host starts session. Can be after taking and processing picture of receipt
 @app.route('/host_login', methods=['POST'])
