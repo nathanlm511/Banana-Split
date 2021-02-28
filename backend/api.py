eee8fe8c2223b47bf12db64600b19d87eb55c052
nathanlm511 <nathanmoeliono@gmail.com>
2021-02-28 05:30:25 -0500

    test

--- backend/api.py
+++ backend/api.py
@@ -74,12 +74,10 @@ def post_image():
 # When host starts session. Can be after taking and processing picture of receipt
 @app.route('/host_login', methods=['POST'])
 def host_login():
-    '''
     venmo_user = request.json["username"]
     venmo_pass = request.json["password"]
-
     try:
-        access_token = Client.get_access_token(username=venmo_user, password=venmo_pass)
+        access_token = Client.get_access_token(username='Sam-Schoedel', password='VTHACKS8KNITTING')
     except:
         print("username or password incorrect")
         response = app.response_class(
@@ -98,6 +96,7 @@ def host_login():
                         "last_name": host.last_name, "display_name": host.display_name, "phone": host.phone,
                         "profile_picture_url": host.profile_picture_url, "about": host.about, 
                         "date_joined": host.date_joined, "is_group": host.is_group, "is_active": host.is_active}
+
     '''   
     
     # Return jsonified data
@@ -105,6 +104,8 @@ def host_login():
                         "last_name": "last", "display_name": "First last", "phone": "+15409052428",
                         "profile_picture_url": "google.com", "about": "about me", 
                         "date_joined": "date_joined", "is_group": True, "is_active": True}
+                        
+                        '''
 
     response = json.dumps(return_data_dict)
 
@@ -218,7 +219,7 @@ def add_user_to_session():
         host_confirm_request(int(session_json["session_id"]))
     else:
         print("nothinggg")
-    return
+    return "done"
 
 def update_connection_user_item(session_id, user, item, item_id, percentage):
     sessions.update_one({"id": session_id, "users": { "$elemMatch": { "name":user}}}, {"$push": {"users.$.bought_items": {"Item ID": item_id, "Name": item, "percent": percentage}}})
@@ -238,13 +239,20 @@ def get_data_from_cursor(session_id):
     session_json = cursor_to_json(sessions.find({"id": session_id}))
     print("------------")
     session_json = json.loads(session_json)[0]
+
     user_list = {}
     item_id = 0
 
     for user in session_json["users"]:
+        price = 0
         username = user["name"]
-        price = (user["bought_items"][item_id]["percent"] / 100) * session_json["items"][item_id]["price"]
+        for item in user["bought_items"]:
+
+            price += (item["percent"] / 100) * session_json["items"][item_id]["price"]
+            item_id += 1
+        
         user_list[username] = price
+        item_id = 0
     
     host_num = session_json["host"]
 
