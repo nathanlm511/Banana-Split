fd443d253ae623df5159799c922fb8d286f5d05c
sschoedel <sschoedel@vt.edu>
2021-02-28 02:43:42 -0500

    send confirmation message over text

--- backend/api.py
+++ backend/api.py
@@ -6,6 +6,7 @@ import pymongo
 from flask_cors import CORS, cross_origin
 import numpy as np
 from bson.json_util import dumps
+from twilio.rest import Client
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -18,6 +19,9 @@ friends = []
 client = pymongo.MongoClient("mongodb+srv://jusjus:jusjus@cluster0.ksh52.mongodb.net/sessions?retryWrites=true&w=majority")
 db = client.db
 sessions = db['sessions']
+
+twilio_client = Client("AC588038219fa0772239a1af667dabd171", "771d5391ee31d506da061a13a39f2721")
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -36,7 +40,6 @@ def post_image():
     
     dummy_data = '{"balance due": "65.32", "all food": [{"name": "ACTIVIA MC BERRY 4PK M", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "B&J FOG BRWNIE ICM", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "B&) STRAW CHSCAKE ICM", "num items": 1, "item_cost": 2.5, "total cost": 2.5, "food group": "FROZEN/DAIRY"}, {"name": "SK WLD AK PINK SLMN Ax", "num items": 1, "item_cost": 4.49, "total cost": 4.49, "food group": "GROCERY"}, {"name": "FL 41-50 RAW SHRIMP M Ax", "num items": 3, "item_cost": 5.49, "total cost": 16.47, "food group": "MEAT"}, {"name": "FL ORIGINAL MEATBALL Ax", "num items": 1, "item_cost": 4.49, "total cost": 4.49, "food group": "MEAT"}, {"name": "BNLS NY STRIP 17S TH A x", "num items": 2, "item_cost": 7.29, "total cost": 15.280000000000001, "food group": "MEAT"}, {"name": "MSSLS GRLC BTTR SCE Ax", "num items": 1, "item_cost": 3.99, "total cost": 3.99, "food group": "MEAT"}, {"name": "GREEN ONIONS", "num items": 6, "item_cost": 0.79, "total cost": 4.74, "food group": "PRODUCE"}, {"name": "ORGANIC CELLO CARROT Ax", "num items": 1, "item_cost": 1.29, "total cost": 1.29, "food group": "PRODUCE"}, {"name": "WHOLE WHITE MUSHROOM A x", "num items": 1, "item_cost": 1.99, "total cost": 1.99, "food group": "PRODUCE"}, {"name": "MUSCADINE GRAPES ORT Ax", "num items": 1, "item_cost": 3.49, "total cost": 3.49, "food group": "PRODUCE"}]}'
     return dummy_data
-
 
 # When host starts session. Can be after taking and processing picture of receipt
 @app.route('/host_login', methods=['POST'])
@@ -111,7 +114,13 @@ def friend_login():
 # After everyone has filled out their forms
 @app.route('/host_confirm_request', methods=['POST'])
 def host_confirm_request():
-    pass
+    # num = "+13303099014"
+    # num = "+15409052428"
+    link_to_page = "https://something"
+    message = "Everyone has filled up their shopping carts! Request your money here: " + link_to_page
+    client.messages.create(to=num, 
+                        from_="+13023004884", 
+                        body=message)
 
 @app.route('/get_session', methods=['POST'])
 def get_session_data():
